991a3b787aab13c0dba116cc3a3e014767879193
Andres Cendales <andres01660@gmail.com>
2020-10-04 16:12:52 -0500

    Organizing models in django package

--- cride/utils/models.py
+++ cride/utils/models.py
@@ -4,28 +4,29 @@
 from django.db import models
 
 class CRideModel(models.Model):
-    """share ride base model.
-
-    CRideModel acts as an abstract basse class from wich every 
-    other model in the project will inherit. This class provides 
-    every table with the following attributes
-        + creted(DateTime) : Store the datetime the object was created
-        + modify(DateTime) : Store the datetime the object was modified
+    """Comparte Ride base model.
+    CRideModel acts as an abstract base class from which every
+    other model in the project will inherit. This class provides
+    every table with the following attributes:
+        + created (DateTime): Store the datetime the object was created.
+        + modified (DateTime): Store the last datetime the object was modified.
     """
-    created = models.DateField(
-        'created at', 
-        auto_now_add=True
-        help_text='Date time on wich the object was created.'
+
+    created = models.DateTimeField(
+        'created at',
+        auto_now_add=True,
+        help_text='Date time on which the object was created.'
     )
-    
-    modified = models.DateField(
-        'modified at', 
-        auto_now=True
-        help_text='Date time on wich the object was last modified.'
+    modified = models.DateTimeField(
+        'modified at',
+        auto_now=True,
+        help_text='Date time on which the object was last modified.'
     )
 
     class Meta:
-        """Meta option"""
+        """Meta option."""
+
         abstract = True
+
         get_latest_by = 'created'
-        ordering = ['-created','-modified']
+        ordering = ['-created', '-modified']
